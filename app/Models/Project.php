522e5412a5e78aea62138f2727cca27a4ec210b2
Filippo Sbaiz <sbaizfilippo@gmail.com>
2023-03-03 17:08:45 +0100

    popolamento con seeder

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,11 +9,14 @@ use Illuminate\Support\Str;
 
 class Project extends Model
 {
-    protected $fillable = ['title', 'content', 'slug'];
+    protected $fillable = ['title', 'content', 'slug', 'type_id'];
 
     use HasFactory;
 
     public static function generateSlug($title){
         return Str::slug($title, '-');
+    }
+    public function type(){
+        return $this->belongsTo(Type::class);
     }
 }
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+use App\Models\Type;
+
+use function PHPSTORM_META\type;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +19,15 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = ['FrontEnd', 'BackEnd', 'Fullstack', 'Design', 'DevOps'];
+
+        foreach($types as $type){
+            $newType = new Type();
+
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name, '-');
+
+            $newType->save();
+        }
     }
 }
